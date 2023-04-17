b7625959368cffb164152402a9bb1450db4cc4af
erenst <erenst2002@hotmail.com>
2023-04-17 20:48:06 +0930

    Community Page (Server-side Rendering with NextJS, data fetching with getServerSideProps)

--- src/pages/r/[communityId]/index.tsx
+++ src/pages/r/[communityId]/index.tsx
@@ -1,9 +1,38 @@
-import { FC } from 'react';
-
-interface Props {}
-
-const index: FC<Props> = (props) => {
-  return <div>index</div>;
+import { Community } from "@/src/atoms/CommunitiesAtom";
+import { firestore } from "@/src/firebase/clientApp";
+import { doc, getDoc } from "firebase/firestore";
+import { GetServerSidePropsContext } from "next";
+import React from "react";
+import safeJsonStringify from "safe-json-stringify";
+type CommunityPageProps = {
+  communityData: Community;
 };
 
-export default index;
+const CommunityPage: React.FC<CommunityPageProps> = ({ communityData }) => {
+  return <div>Welcome To {communityData.id}</div>;
+};
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+  // get community data and pass it to client
+  try {
+    const communityDocRef = doc(
+      firestore,
+      "communities",
+      context.query.communityId as string
+    );
+    const communityDoc = await getDoc(communityDocRef);
+    return {
+      props: {
+        communityData: JSON.parse(
+          safeJsonStringify({
+            id: communityDoc.id,
+            ...communityDoc.data(),
+          })
+        ),
+      },
+    };
+  } catch (error) {
+    //Could add error page here
+    console.log("getServerSideProps error", error);
+  }
+}
+export default CommunityPage;
