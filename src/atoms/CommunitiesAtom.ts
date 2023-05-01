8ed2dfb9b748741dc0e2220542a753f5e7b0c813
erenst <erenst2002@hotmail.com>
2023-05-01 20:50:45 +0930

    Community Data Recoil Atom (global communities state)

--- src/atoms/CommunitiesAtom.ts
+++ src/atoms/CommunitiesAtom.ts
@@ -9,3 +9,19 @@ export interface Community {
   createdAt?: Timestamp;
   imageURL?: string;
 }
+interface CommunitySnippet {
+  communityId: string;
+  isModerator?: boolean;
+  imageURL?: string;
+}
+interface CommunityState {
+  mySnippet: CommunitySnippet[];
+}
+
+const defaultCommunityState: CommunityState = {
+  mySnippet: [],
+};
+export const communityState = atom<CommunityState>({
+  key: "communityState",
+  default: defaultCommunityState,
+});
