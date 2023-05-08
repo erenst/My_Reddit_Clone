974b0c72320ae7d7b094a3dc77682650ae5dc510
erenst <erenst2002@hotmail.com>
2023-05-08 20:54:54 +0930

    PostTextInputs (Post Title & Body)

--- src/components/Posts/NewPostForm.tsx
+++ src/components/Posts/NewPostForm.tsx
@@ -4,6 +4,7 @@ import { BsLink45Deg, BsMic } from "react-icons/bs";
 import { IoDocumentText, IoImageOutline } from "react-icons/io5";
 import { BiPoll } from "react-icons/bi";
 import SingleTabItem from "./SingleTabItem";
+import TextInputs from "./PostForm/TextInputs";
 type NewPostFormProps = {};
 
 const formTabs: TabItem[] = [
@@ -34,6 +35,26 @@ export type TabItem = {
 };
 const NewPostForm: React.FC<NewPostFormProps> = () => {
   const [selectedTab, setSelectedTab] = useState(formTabs[0].title);
+  const [textInputs, setTextInputs] = useState({
+    title: "",
+    body: "",
+  });
+  const [selectedFile, setSelectedFile] = useState<string>();
+  const [loading, setLoading] = useState(false);
+
+  const handleCreatePost = async () => {};
+  const onSelectImage = () => {};
+  const onTextChange = (
+    event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    const {
+      target: { name, value },
+    } = event;
+    setTextInputs((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
   return (
     <Flex direction="column" bg="white" borderRadius={4} mt={2}>
       <Flex width="100%">
@@ -44,6 +65,16 @@ const NewPostForm: React.FC<NewPostFormProps> = () => {
             setSelectedTab={setSelectedTab}
           />
         ))}
+      </Flex>
+      <Flex p={4}>
+        {selectedTab === "Post" && (
+          <TextInputs
+            textInputs={textInputs}
+            handleCreatePost={handleCreatePost}
+            onChange={onTextChange}
+            loading={loading}
+          />
+        )}
       </Flex>
     </Flex>
   );
---
+++ src/components/Posts/PostForm/TextInputs.tsx
@@ -0,0 +1,78 @@
+import { Button, Flex, Input, Stack, Textarea } from "@chakra-ui/react";
+import React from "react";
+
+type TextInputsProps = {
+  textInputs: {
+    title: string;
+    body: string;
+  };
+  onChange: (
+    event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => void;
+  handleCreatePost: () => void;
+  loading: boolean;
+};
+
+const TextInputs: React.FC<TextInputsProps> = ({
+  textInputs,
+  onChange,
+  handleCreatePost,
+  loading,
+}) => {
+  return (
+    <Stack spacing={3} width="100%">
+      <Input
+        name="title"
+        value={textInputs.title}
+        onChange={onChange}
+        fontSize="10pt"
+        borderRadius={4}
+        placeholder="Title"
+        _placeholder={{
+          color: "gray.500",
+        }}
+        // use _focusVisible to change outline color
+        _focusVisible={{
+          outline: "none",
+        }}
+        _focus={{
+          bg: "white",
+          border: "1px solid",
+          borderColor: "black",
+        }}
+      />
+      <Textarea
+        name="body"
+        value={textInputs.body}
+        fontSize="10pt"
+        borderRadius={4}
+        height="100px"
+        placeholder="Text (optinal)"
+        _placeholder={{
+          color: "gray.500",
+        }}
+        _focusVisible={{
+          outline: "none",
+        }}
+        _focus={{
+          bg: "white",
+          border: "1px solid",
+          borderColor: "black",
+        }}
+        onChange={onChange}
+      />
+      <Flex justify="flex-end">
+        <Button
+          height="34px"
+          padding="0px 30px"
+          disabled={!textInputs.title}
+          isLoading={loading}
+          onClick={handleCreatePost}
+        >
+          Post
+        </Button>
+      </Flex>
+    </Stack>
+  );
+};
+export default TextInputs;
