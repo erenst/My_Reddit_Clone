0f4d5aeed48fb84de30c267958fc48853d0dce44
erenst <erenst2002@hotmail.com>
2023-05-14 15:43:08 +0930

    handleCreatePost Function (Save Posts to Firestore, Upload Image to Firebase Storage)

--- src/components/Posts/NewPostForm.tsx
+++ src/components/Posts/NewPostForm.tsx
@@ -1,4 +1,4 @@
-import { Flex, Icon } from "@chakra-ui/react";
+import { Alert, AlertIcon, Flex, Icon, Text } from "@chakra-ui/react";
 import React, { useState } from "react";
 import { BsLink45Deg, BsMic } from "react-icons/bs";
 import { IoDocumentText, IoImageOutline } from "react-icons/io5";
@@ -6,7 +6,21 @@ import { BiPoll } from "react-icons/bi";
 import SingleTabItem from "./SingleTabItem";
 import TextInputs from "./PostForm/TextInputs";
 import ImageUpload from "./PostForm/ImageUpload";
-type NewPostFormProps = {};
+import { Post } from "@/src/atoms/postsAtom";
+import { User } from "firebase/auth";
+import { useRouter } from "next/router";
+import {
+  Timestamp,
+  addDoc,
+  collection,
+  serverTimestamp,
+  updateDoc,
+} from "firebase/firestore";
+import { firestore, storage } from "@/src/firebase/clientApp";
+import { getDownloadURL, ref, uploadString } from "firebase/storage";
+type NewPostFormProps = {
+  user: User;
+};
 
 const formTabs: TabItem[] = [
   {
@@ -34,7 +48,8 @@ export type TabItem = {
   title: string;
   icon: typeof Icon.arguments;
 };
-const NewPostForm: React.FC<NewPostFormProps> = () => {
+const NewPostForm: React.FC<NewPostFormProps> = ({ user }) => {
+  const router = useRouter();
   const [selectedTab, setSelectedTab] = useState(formTabs[0].title);
   const [textInputs, setTextInputs] = useState({
     title: "",
@@ -42,8 +57,43 @@ const NewPostForm: React.FC<NewPostFormProps> = () => {
   });
   const [selectedFile, setSelectedFile] = useState<string>();
   const [loading, setLoading] = useState(false);
-
-  const handleCreatePost = async () => {};
+  const [error, setError] = useState(false);
+  const handleCreatePost = async () => {
+    const { communityId } = router.query;
+    //1.create new post object =>Type Post
+    const newPost: Post = {
+      communityId: communityId as string,
+      creatorId: user.uid,
+      creatorDisplayName: user.email!.split("@")[0],
+      title: textInputs.title,
+      body: textInputs.body,
+      numberOfComments: 0,
+      voteStatus: 0,
+      createdAt: serverTimestamp() as Timestamp,
+    };
+    setLoading(true);
+    //2.store post in db
+    try {
+      const postDocRef = await addDoc(collection(firestore, "posts"), newPost);
+      //3.check for selectedFile
+      if (selectedFile) {
+        //3.1 store in storage =>getDownloadURL(return imageUrl)
+        const imageRef = ref(storage, `posts/${postDocRef.id}/image`);
+        await uploadString(imageRef, selectedFile, "data_url");
+        const downloadURL = await getDownloadURL(imageRef);
+        //3.2 update post doc by add imageUrl
+        await updateDoc(postDocRef, {
+          imageURL: downloadURL,
+        });
+      }
+    } catch (error: any) {
+      console.log("handleCreatePostError", error.message);
+      setError(true);
+    }
+    setLoading(false);
+    //4.redirect the user back to communityPage using router
+    router.back();
+  };
   const onSelectImage = (event: React.ChangeEvent<HTMLInputElement>) => {
     const reader = new FileReader();
     if (event.target.files?.[0]) {
@@ -96,6 +146,12 @@ const NewPostForm: React.FC<NewPostFormProps> = () => {
           />
         )}
       </Flex>
+      {error && (
+        <Alert status="error">
+          <AlertIcon />
+          <Text>Some error has happened when you are creating your post. </Text>
+        </Alert>
+      )}
     </Flex>
   );
 };
--- src/pages/r/[communityId]/submit.tsx
+++ src/pages/r/[communityId]/submit.tsx
@@ -1,16 +1,19 @@
+import { useAuthState } from "react-firebase-hooks/auth";
 import PageContent from "../../../components/Layout/PageContent";
 import NewPostForm from "../../../components/Posts/NewPostForm";
 import { Box, Text } from "@chakra-ui/react";
 import React from "react";
+import { auth } from "@/src/firebase/clientApp";
 
 const SubmitPostPage: React.FC = () => {
+  const [user] = useAuthState(auth);
   return (
     <PageContent>
       <>
         <Box p="14px 0px" borderBottom="1px solid" borderColor="white">
           <Text>Create a post</Text>
         </Box>
-        <NewPostForm />
+        {user && <NewPostForm user={user} />}
       </>
       <></>
     </PageContent>
